c7ee38b1839043df085e558ef983eed7d87858aa
AlexKolokolov <kolokolov_a@ukr.net>
2017-02-24 14:52:57 +0200

    Change output text to more appropriate

--- app/view/DepartmentView.js
+++ app/view/DepartmentView.js
@@ -2,6 +2,6 @@ Ext.define('ExtJSRestClient.view.DepartmentView', {
     extend: 'Ext.Panel',
     config: {
         fullscreen: true,
-        tpl: '<p>The ID is {id}</p><p>The name of department is {name}</p>'
+        tpl: '<p>Department ID is {id}</p><p>Department name is {name}</p>'
     }
 });
